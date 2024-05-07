9e6be95b30639ee54d50691348df4b942f15a2fe
Jurgsl <60768303+Jurgsl@users.noreply.github.com>
2024-05-08 10:05:33 +1200

    Add files via upload
    These are variables examples

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Showing how variables work
+# Juergen Lier
+# 3 May 2024Year 10 demo
+
+
+
+# sum = "5" + "5" # Adding strings together is called concatenation
+# print(sum)
+
+# sum = 5 + 5
+# print(sum)
+
+keep_going = ""
+while keep_going == "":
+    print("Looping")
+    print("still looping")
+
+    keep_going = input("Again?")
+    
+print("all done")
